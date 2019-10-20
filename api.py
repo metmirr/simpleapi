726815f0ca1828ee6ffdd1b1905f608e27e178b9
metmirr <metin7087@gmail.com>
2019-10-20 16:10:47 +0300

    Format code with black

--- api.py
+++ api.py
@@ -8,13 +8,13 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///data.sqlite')
+engine = create_engine("sqlite:///data.sqlite")
 session = sessionmaker(engine)()
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(64))
     email = Column(String(64), unique=True)
@@ -25,9 +25,9 @@ class Index(object):
         self.d = OrderedDict()
 
     def on_get(self, req, resp):
-        self.d['message'] = "It's a simple api written with falcon"
-        self.d['db'] = 'sqlite'
-        self.d['orm'] = 'sqlalchemy'
+        self.d["message"] = "It's a simple api written with falcon"
+        self.d["db"] = "sqlite"
+        self.d["orm"] = "sqlalchemy"
 
         resp.body = json.dumps(self.d)
 
@@ -38,12 +38,17 @@ class UserResource(object):
         self.d = OrderedDict()
 
     def on_get(self, req, resp):
-        user = session.query(User).filter(
-            (User.username == req.get_param('username'))
-            | (User.email == req.get_param('email'))).first()
+        user = (
+            session.query(User)
+            .filter(
+                (User.username == req.get_param("username"))
+                | (User.email == req.get_param("email"))
+            )
+            .first()
+        )
         if user:
-            self.d['username'] = user.username
-            self.d['email'] = user.email
+            self.d["username"] = user.username
+            self.d["email"] = user.email
             resp.body = json.dumps(self.d)
             resp.status = falcon.HTTP_200
         else:
@@ -51,35 +56,43 @@ class UserResource(object):
             resp.status = falcon.HTTP_404
 
     def on_post(self, req, resp):
-        username = req.get_param('username')
-        email = req.get_param('email')
+        username = req.get_param("username")
+        email = req.get_param("email")
 
-        user = session.query(User).filter(
-            (User.username == username) | (User.email == email)).first()
+        user = (
+            session.query(User)
+            .filter((User.username == username) | (User.email == email))
+            .first()
+        )
         if not user and username and email:
             user = User(username=username, email=email)
             session.add(user)
             session.commit()
-            resp.body = 'User has been created.'
+            resp.body = "User has been created."
             resp.status = falcon.HTTP_302
         else:
-            resp.body = b'username or email already in use.'
+            resp.body = b"username or email already in use."
             resp.status = falcon.HTTP_200
 
     def on_delete(self, req, resp):
-        user = session.query(User).filter(
-            (User.username == req.get_param('username'))
-            & (User.email == req.get_param('email'))).first()
+        user = (
+            session.query(User)
+            .filter(
+                (User.username == req.get_param("username"))
+                & (User.email == req.get_param("email"))
+            )
+            .first()
+        )
         if user:
             session.delete(user)
             session.commit()
-            resp.data = b'user deleted.'
+            resp.data = b"user deleted."
             resp.status = falcon.HTTP_200
         else:
-            resp.data = b'user not found.'
+            resp.data = b"user not found."
             resp.status = falcon.HTTP_404
 
 
 api = falcon.API()
-api.add_route('/', Index())
-api.add_route('/user', UserResource())
+api.add_route("/", Index())
+api.add_route("/user", UserResource())
--- manage.py
+++ manage.py
@@ -14,38 +14,39 @@ def cli():
 
 @cli.command()
 def runserver():
-    run_simple('localhost', 5000, api,
-               use_debugger=True, use_reloader=True)
+    run_simple("localhost", 5000, api, use_debugger=True, use_reloader=True)
 
 
 @cli.command()
 def initdb():
-    if os.path.isfile('data.sqlite'):
-        click.echo("""You have database file in the current directory
-            to createad it run dropdb, initdb commands respectively.""")
+    if os.path.isfile("data.sqlite"):
+        click.echo(
+            """You have database file in the current directory
+            to createad it run dropdb, initdb commands respectively."""
+        )
     else:
-        click.echo('initializing database...')
+        click.echo("initializing database...")
         Base.metadata.create_all(engine)
 
 
 @cli.command()
 def dropdb():
-    filename = 'data.sqlite'
+    filename = "data.sqlite"
     if os.path.isfile(filename):
         os.remove(filename)
-        click.echo('dropping database...')
+        click.echo("dropping database...")
     else:
         click.echo("The database doesn't exists")
 
 
 @cli.command()
 def test():
-    click.echo('running tests...')
+    click.echo("running tests...")
     import unittest
 
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
